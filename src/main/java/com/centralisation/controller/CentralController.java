452c606e0fe4c5596409edfbff1bd707c2f1d8c5
mvinet <vinetmanon85@gmail.com>
2024-12-11 15:11:41 +0100

    Controller + service

--- src/main/java/com/centralisation/controller/CentralController.java
+++ src/main/java/com/centralisation/controller/CentralController.java
@@ -1,9 +1,16 @@
 package com.centralisation.controller;
 
+import com.centralisation.model.dto.AirportDTO;
+import com.centralisation.model.dto.FlightDTO;
 import com.centralisation.service.CentralService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -11,4 +18,36 @@ import org.springframework.web.bind.annotation.*;
 public class CentralController {
 
     private final CentralService centralService;
+
+    /**
+     * Récupère tous les vols
+     *
+     * @return les vols disponibles
+     */
+    @GetMapping
+    public List<FlightDTO> getAllFlight() {
+        return centralService.getAllFlight();
+    }
+
+    /**
+     * Récupère tous les aeroports
+     *
+     * @return les aeroports disponibles
+     */
+    @GetMapping
+    public List<AirportDTO> getAllAirport() {
+        return centralService.getAllAirport();
+    }
+
+    /**
+     * Ajoute des vols mis à jour
+     *
+     * @return les vols mis à jour
+     */
+    @PostMapping
+    public List<FlightDTO> pushFlight(@RequestBody List<FlightDTO> flightDTOList) {
+        return centralService.pushFlight(flightDTOList);
+    }
+
+
 }
--- src/main/java/com/centralisation/service/CentralService.java
+++ src/main/java/com/centralisation/service/CentralService.java
@@ -1,9 +1,41 @@
 package com.centralisation.service;
 
+import com.centralisation.model.dto.AirportDTO;
+import com.centralisation.model.dto.FlightDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CentralService {
+
+    /**
+     * Retourne la liste des vols
+     *
+     * @return la liste des vols
+     */
+    public List<FlightDTO> getAllFlight() {
+        // TODO
+    }
+
+    /**
+     * Retourne la liste des aeroports
+     *
+     * @return la liste des aéroports
+     */
+    public List<AirportDTO> getAllAirport() {
+        // TODO
+    }
+
+    /**
+     * Ajoute des vols mis à jour
+     *
+     * @return la liste des aéroports
+     */
+    public List<FlightDTO> pushFlight(List<FlightDTO> flightDTOList) {
+        // TODO
+    }
+
 }
