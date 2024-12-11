8a3074ff51ceb2bedc10173f22e799ab9159214d
jacopo <bonardi14@gmail.com>
2024-12-11 16:19:07 +0100

    update flight controller with 2 endpoints

--- src/main/java/com/centralisation/controller/FlightController.java
+++ src/main/java/com/centralisation/controller/FlightController.java
@@ -8,10 +8,36 @@ public class FlightController {
             if (flightRequest.getAllFlights() == null || flightRequest.getAllFlights().isEmpty()) {
                 return ResponseEntity.badRequest().body(Map.of("error", "Invalid request format"));
             }
+            
+         // enregistrer les vols dans le cache
+            
+            
          // Retourner une réponse de succès
             return ResponseEntity.ok(Map.of(
                 "message", "Flights data successfully updated",
                 "updatedFlights", updatedFlights
+            ));
+        } catch (Exception e) {
+            // Gérer les erreurs
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Internal server error"));
+        }
+    }
+	
+	@GetMapping("/others")
+    public ResponseEntity<?> getOtherFlights(@RequestParam String siteId) {
+        try {
+            // Validation du siteId
+            if (siteId == null || siteId.isBlank()) {
+                return ResponseEntity.badRequest().body(Map.of("error", "Invalid siteId"));
+            }
+
+            // Récupération des vols des autres sites depuis le chache
+            
+
+            // Retourner les vols
+            return ResponseEntity.ok(Map.of(
+                "siteId", siteId,
+                "flights", otherFlights
             ));
         } catch (Exception e) {
             // Gérer les erreurs
