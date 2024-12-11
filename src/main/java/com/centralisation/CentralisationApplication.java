828ee20011b955d4dadc33a4f6c44d5423db3ad5
GregMac <gregorypotesgonzalez@gmail.com>
2024-12-11 15:28:52 +0100

    added enablefeign into application

--- src/main/java/com/centralisation/CentralisationApplication.java
+++ src/main/java/com/centralisation/CentralisationApplication.java
@@ -2,8 +2,10 @@ package com.centralisation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CentralisationApplication {
 
 	public static void main(String[] args) {
